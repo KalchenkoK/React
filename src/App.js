e5052601523cc16c5be06759a0316d2bc9f81a27
KalchenkoK <7584663@gmail.com>
2022-02-28 12:36:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  return(
+    <header>
+      <h1>My first React App</h1>
+    </header>
+  )
+}
+
+function Container(){
+  return(
+    <div>
+      <div>
+        <p>Here is main content</p>
+      </div>
+    </div>
+  )
+}
+function Footer(){
+  return(
+    <footer>
+      <p>Designed by Konstantin Kalchenko</p>
+      <div>
+        <p>Our contacts</p>
+        <p><a href="https://facebook.com">Facebook </a></p>
+        <p><a href="https://instagram.com">Instagram</a></p>
+        <p><a href="https://linkedin.com">Linkedin</a></p>
+      </div>
+    </footer>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Container/>
+      <Footer/>      
+    </div>
+  );
+}
+
+export default App;
