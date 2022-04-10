3d0f95753a919a44d88fcbe3ab9bd7985400e3ed
KalchenkoK <7584663@gmail.com>
2022-04-10 19:09:58 +0300

    hw_5_1

--- hw_5/src/hooks/common.js
+++ hw_5/src/hooks/common.js
@@ -2,19 +2,15 @@ import { useState } from "react";
 
 export function useAsync(cb, defaultValue = []) {
   const [cards, setCards] = useState(defaultValue);
-  // const [data, setData] = useState(defaultValue);
+  
 
   function run() {
-    //   setStatus("LOADING");
-
     cb()
       .then((cards) => {
-        //   setStatus("DONE");
         setCards(cards);
         return cards;
       })
       .catch((error) => {
-        // setStatus("ERROR");
         return Promise.reject(error);
       });
   }
